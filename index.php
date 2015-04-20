97a621176dbbfeb318bde86908f13019a9167208
Raul Ramirez <raul287986@gmail.com>
2015-04-19 19:43:10 -0700

    video 15

--- index.php
+++ index.php
@@ -8,7 +8,25 @@
 	<div class="wrap">
 	  <div class="task-list">
 	  	<ul>
-	  		<?php require ("includes/connect.php"); ?>
+	  		<?php require ("includes/connect.php"); 
+  			$mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
+        $query = "SELECT * FROM tasks ORDER BY date ASC, time ASC";
+ 				if ($result = $mysqli->query($query)) {
+          $numrows = $result->num_rows:
+          if ($numrows>0) {
+          	while($row = $result->fetch_assoc()){
+          		$task_id = $row['id'];
+          		$task_name = $row["task"];
+
+          		echo "<li>
+          		<span>'.$task_name'
+          		";
+          	}
+          }
+ 				}
+
+
+        ?>
 	  	</ul>
 		</div>
 	<form class="add-new-task" autocomplete="off">
@@ -33,5 +51,16 @@
 		return false;
 		});
 	}
+
+	$('.delete-button').click(function(){
+		var current_element = $(this);
+		var task_id = $(this).attr('id');
+
+		$.post('includes/delete-task.php', {id: task_id}, function(){
+		current_element.parent().fadeOut("fast", function(){
+			$(this).remove();
+		});
+	});
+});
 </script>
 </html>
